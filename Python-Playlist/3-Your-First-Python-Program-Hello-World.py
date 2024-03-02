3fd7c1b6b59f528372886a505314566982541238
himanshu1415 <1415himanshu1415@gmail.com>
2024-03-02 11:22:59 +0530

    Added new file

---
+++ Python-Playlist/3-Your-First-Python-Program-Hello-World.py
@@ -0,0 +1,14 @@
+def firstProgram():
+    welcomeText = "Hello World This is my first program in python"
+    #print(welcomeText)
+
+    if 10 > 2:
+        print("10 is greater than two!")
+    x=2
+    print(x)
+    y="sample variable declaration"
+    """This is a comment.
+    
+        multi line comment
+        """
+firstProgram()
