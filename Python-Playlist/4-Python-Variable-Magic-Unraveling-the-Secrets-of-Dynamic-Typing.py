91a1b5635fe6429566a84f0e9c26b9317ddc9ca5
himanshu1415 <1415himanshu1415@gmail.com>
2024-03-02 11:54:43 +0530

    Added new file

---
+++ Python-Playlist/4-Python-Variable-Magic-Unraveling-the-Secrets-of-Dynamic-Typing.py
@@ -0,0 +1,38 @@
+def variableDeclarationProgram():
+    #Part 1
+    """x = 2
+    y = "Himanshu"
+    print(x)
+    print(y)"""
+
+    #Change Type
+    """x = 2      # x is of type int
+    x = "Himanshu" # x is now of type str
+    print(x)"""
+
+    #Casting
+    """x = str(2)
+    y = int(2)    
+    z = float(2)  
+    print(x)
+    print(y)
+    print(z)"""
+    
+    #Get Type
+    """x = 2.0
+    y = "Himanshu"
+    print(type(x))
+    print(type(y))"""
+
+    #Single or double Quotes
+    """x = "Himanshu"
+    # is the same as
+    x = 'Himanshu'"""
+
+    #Case Sensitive
+    """aa = 4
+    2A = "Himanshu"
+    print(a)
+    print(A)"""
+
+variableDeclarationProgram()   
