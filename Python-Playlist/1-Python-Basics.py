33615034ee231a0f9970b23b9bd31bbd2e86b8fd
himanshu1415 <1415himanshu1415@gmail.com>
2024-03-02 11:06:38 +0530

    Added Why Python is Recommend

---
+++ Python-Playlist/1-Python-Basics.py
@@ -0,0 +1,9 @@
+def pythonBasics():
+    whatIsPython = "Python is a high-level, general-purpose, and very popular programming language"
+    whyPython = "huge collection of standary library which can be used for machine learning"
+    whyRecommendedForBeginners = "Easy to learn and easy to debug"
+    print("What is Python "+whatIsPython)
+    print("Why Python "+whyPython)
+    print("Why Python is recommended for beginners  "+whyRecommendedForBeginners)
+
+pythonBasics()    
