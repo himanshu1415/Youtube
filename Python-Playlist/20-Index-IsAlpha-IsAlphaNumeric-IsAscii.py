9b527af5c604e4e3364f34e75a719c060f2c1282
himanshu1415 <1415himanshu1415@gmail.com>
2024-03-19 21:15:04 +0530

    20-March-2024 Video Added

---
+++ Python-Playlist/20-Index-IsAlpha-IsAlphaNumeric-IsAscii.py
@@ -0,0 +1,24 @@
+def stringMoreFunctions():
+    # Index
+    text = "Subscribe CodersWorld"
+    print(text.index('Coder'))
+    #print(text.index('Content'))
+    #print(text.index('CodersWorld',0))
+    #print(text.index('s',9,30))
+    #print(text.index('CodersWorld',-12))
+    
+    # IsAlnum
+    #text = "alpanumeric123AL"   
+    #text = "-# 123 AL"
+    #print(text.isalnum())
+
+    # IsAlpha
+    #text = "abcd"
+    text = "abcd1"
+    #print(text.isalpha())
+
+    # IsAscii
+    #text = "abcd#!!@#$%^&*() "
+    text = "ß"
+    #print(text.isascii())
+stringMoreFunctions()
