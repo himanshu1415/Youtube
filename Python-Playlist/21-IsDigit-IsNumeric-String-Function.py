0240ec6601c839a18c931aa2af51975074c8be8b
himanshu1415 <1415himanshu1415@gmail.com>
2024-04-16 22:13:10 +0530

    17-April-Code-Updated

---
+++ Python-Playlist/21-IsDigit-IsNumeric-String-Function.py
@@ -0,0 +1,21 @@
+def stringMoreFunctions():
+    # isDigit
+    text = "123"   
+    #text = "a123"
+    #text = "\u0030"   #0  
+    #text = "\u00B2"  #E^2 
+    #text = "-123"
+    #text = "1.23"
+    #text = "ↁ" # roman numeral
+    print(text.isdigit())
+
+    # isnumeric
+    #text = "123"   
+    #text = "a123"
+    #text = "⅔"
+    #text = "2/3"
+    #text = "2²"
+    #text = "ↁ"
+    #print(text.isnumeric())
+
+stringMoreFunctions()
