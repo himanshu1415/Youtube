bf9223b3946cb4bd12205d7c596f23c9f8026cbf
himanshu1415 <1415himanshu1415@gmail.com>
2024-05-01 20:25:41 +0530

    1-may-2024 video code added

---
+++ Python-Playlist/23-Operators-TypesOfOperators-ArithmeticOperators.py
@@ -0,0 +1,22 @@
+def operationInPython():
+    #Operators  in Python
+    #Types of Operators 
+    """
+    Arithmetic operators
+    Assignment operators
+    Comparison operators
+    Logical operators
+    Identity operators
+    Membership operators
+    Bitwise operators
+    """
+
+    # Arithmetic Operators
+    variable_1 = 9
+    variable_2 = 2
+    print(variable_1 + variable_2)   # + operator example
+    #print(variable_1 - variable_2)  # - operator example
+    #print(variable_1 * variable_2)  # * operator example
+    #print(type(variable_1 / variable_2))  # / operator example
+
+operationInPython()
