d85ef7aa4259499ddf2eb20f4c020409f8aab2f9
himanshu1415 <1415himanshu1415@gmail.com>
2024-05-14 21:33:11 +0530

    15-May-2024-CodeAdded

---
+++ Python-Playlist/25-AssignmentOperators-Part-1.py
@@ -0,0 +1,12 @@
+def AssignmentOperator():
+    # Assignment Operators
+    variable_1 = 4     # = operator example
+    #variable_1 += 2   # += operator example     
+    #variable_1 -= 2   # -= operator example
+    #variable_1 *= 2   # *= operator example
+    #variable_1 /= 2   # /= operator example 
+    #variable_1 %= 2   # %= operator example      
+    #variable_1 //= 2  # //= operator example
+    #variable_1 **= 2  # **= operator example
+    print(variable_1) 
+AssignmentOperator()
