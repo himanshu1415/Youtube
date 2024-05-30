260bc991b792f690e393d45b265a3a7d1e79dd23
himanshu1415 <1415himanshu1415@gmail.com>
2024-05-30 08:10:40 +0530

    30-May-Code-Updated

---
+++ Python-Playlist/27-AssignmentOperators-Part-3.py
@@ -0,0 +1,34 @@
+def AssignmentOperator():
+    variable_1 = 4
+    # Binary No Representation
+    #  16 8 4 2 1
+    #         1 0 ( 2  Represented in Binary )   
+    #       1 0 0 ( 4  Represented in Binary )
+    #     1 0 1 0 ( 10 Represented in Binary )
+    # Represent 7 in Binary 
+           # 1 0 0
+           # 1 1 0
+           # 1 1 0
+    
+    #variable_1 &= 5    #Same as variable_1 = variable_1 & 2  
+    #variable_1 |= 2    #Same as variable_1 = variable_1 | 2  
+    #  XOR    &  |  ^
+    #    11   1  1  0
+    #    10   0  1  1
+    #    01   0  1  1 
+    #    00   0  0  0
+        
+    #    100
+    #    010
+    #    110
+
+    #   101
+    #  1010
+    # 10100      
+    #variable_1 ^= 6    #Same as variable_1 = variable_1 ^ 2
+    variable_1 = 5
+    #variable_1 >>= 3   #Same as variable_1 = variable_1 >> 2 
+    variable_1 <<= 2    #Same as variable_1 = variable_1 << 2    
+    print(variable_1) 
+
+AssignmentOperator()
