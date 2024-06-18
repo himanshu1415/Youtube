6279586c3cba3afe79d6cb02568cde2198e7a75b
himanshu1415 <1415himanshu1415@gmail.com>
2024-06-18 21:40:58 +0530

    Membership operators in Python Added

---
+++ Python-Playlist/29-MembershipOperators.py
@@ -0,0 +1,10 @@
+def MembershipOperator():
+    x = ["Like","Share","Subscribe","CodersWorld"]
+    print("Channel" in x)
+	
+    #print("Comment" not in x)
+
+    y = "Comment is Need for Feedback" 
+    #print("Comment" in y)
+
+MembershipOperator()
